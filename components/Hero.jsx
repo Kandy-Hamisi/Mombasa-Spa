1307266517283c33c815935a08a00f34abd734ca
Kandy-Hamisi <hkandy2000@gmail.com>
2022-09-20 21:18:17 +0300

    :wrench: removed left margin in mobile devices

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Hero = () => {
   return (
     <section className='bg-salon w-full'>
-        <div className='flex pt-[4rem] flex-col text-center lg:text-left lg:items-start items-center justify-center  min-h-screen ml-16'>
+        <div className='flex pt-[4rem] flex-col text-center lg:text-left lg:items-start items-center justify-center  min-h-screen lg:ml-16 lg:ml-0'>
             <div className='space-y-4'>
                 <div className=''>
                     <h1 className='text-[3rem] text-white max-w-sm font-bold'>A Smooth Salon Experience in Your Town</h1>
