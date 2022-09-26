38446bf37ca9839ad0f70267dfe44977d6149017
Kandy-Hamisi <hkandy2000@gmail.com>
2022-09-26 14:12:23 +0300

    :heavy_plus_sign: added and worked on the product page; can add product dynamically

---
+++ constants/allProducts.js
@@ -0,0 +1,34 @@
+export const products = [
+    {
+        id: 1,
+        url: "https://cdn.pixabay.com/photo/2021/05/20/04/36/perfume-6267705_960_720.jpg",
+        name: "Le Grande Perfume",
+        rating: 5,
+        price: 199.99,
+        alt: "product 1"
+    },
+    {
+        id: 2,
+        url: "https://cdn.pixabay.com/photo/2019/12/27/10/01/flowers-4722095_960_720.jpg",
+        name: "Strawberry Epic Essential Oil",
+        rating: 5,
+        price: 59.00,
+        alt: "product 2"
+    },
+    {
+        id: 3,
+        url: "https://cdn.pixabay.com/photo/2018/01/10/13/47/essential-oil-3073901_960_720.jpg",
+        name: "Essential Oil Kiwi",
+        rating: 5,
+        price: 34.90,
+        alt: "product 3"
+    },
+    {
+        id: 4,
+        url: "https://cdn.pixabay.com/photo/2018/11/15/02/41/essential-oil-3816410_960_720.jpg",
+        name: "Essential Oil Lavender",
+        rating: 5,
+        price: 89.00,
+        alt: "product 4"
+    },
+]
